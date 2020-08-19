977b28ab21dbb3ce7c0c239a28d4893528b93d83
PROgienek <jakobiakmikolaj@gmail.com>
2020-08-19 11:35:06 +0200

    Input with Icon finished

--- components/Atoms/Inputs/Input.js
+++ components/Atoms/Inputs/Input.js
@@ -1,8 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 
-function _Input({ value, className }) {
-  return (
+function _Input({ value, className, icon: Icon, width, height }) {
+  return Icon ? (
+    <span className={className}>
+      <input value={value} />
+      <span className="underline" />
+      <div className="blur" />
+      <Icon color="white" />
+    </span>
+  ) : (
     <span className={className}>
       <input value={value} />
       <span className="underline" />
@@ -15,9 +22,10 @@ const whitegrey = "#ececec";
 
 const Input = styled(_Input)`
   position: relative;
-  > input {
-    width: 200px;
+  display: inline-block;
+  height: ${({ height }) => height};
 
+  > input {
     font-size: 0.75rem;
     letter-spacing: 0.1em;
     color: ${whitegrey};
@@ -26,6 +34,8 @@ const Input = styled(_Input)`
     border-radius: 8px;
 
     padding: 0.8em 1em;
+    width: ${({ width }) => width};
+    height: calc(100% - 1.8em);
     cursor: text;
 
     transition: background 600ms ease-in-out, color 600ms ease-in-out;
@@ -42,22 +52,71 @@ const Input = styled(_Input)`
     }
   }
 
+  > svg {
+    height: ${({ height }) => height};
+    position: absolute;
+    right: 8px;
+    z-index: 2;
+    visibility: visible;
+    opacity: 1;
+    transition: visibility 600s, opacity 600ms ease-in-out;
+  }
+
+  .blur {
+    height: inherit;
+    width: ${({ width }) => width};
+    position: absolute;
+    background: linear-gradient(
+      90deg,
+      rgba(51, 51, 51, 0) 0%,
+      rgba(51, 51, 51, 0) 60%,
+      rgba(51, 51, 51, 1) 85%,
+      rgba(51, 51, 51, 1) 100%
+    );
+    right: 0;
+    top: 0;
+    border-radius: 8px;
+    pointer-events: none;
+    visibility: visible;
+    opacity: 1;
+    transition: visibility 600ms, opacity 300ms ease-in-out;
+  }
+
+  input:focus ~ .blur,
+  input:focus ~ svg {
+    visibility: hidden;
+    opacity: 0;
+    transition: visibility 900ms, opacity 600ms ease-in-out;
+  }
+
   .underline {
     position: absolute;
-    bottom: -2px;
-    left: 0.8em;
-    width: calc(100% - 1.6em);
+
     height: 1px;
-    transition: background 600ms ease-in-out;
+    width: calc(90% - 1.6em);
+    bottom: 8px;
+    left: 0.8em;
+    z-index: 1;
+
+    transition: background 600ms ease-in-out, width 600ms ease-in-out;
+
     background: ${darkgrey};
     background-image: linear-gradient(45deg, transparent 50%, ${whitegrey} 50%);
-    background-size: 400%;
     background-position: 100%;
+    background-size: 400%;
   }
 
   input:focus + .underline {
     background-position: 25%;
+    width: calc(100% - 1.6em);
   }
 `;
+
+Input.defaultProps = {
+  value: "Kurwa, w kurwe długi tekst. drtfgyhuijkoplkojihugyftdrsftgyhuji",
+  icon: null,
+  width: "200px",
+  height: "40px",
+};
 
 export default Input;
--- components/Atoms/Inputs/Inputs.stories.js
+++ components/Atoms/Inputs/Inputs.stories.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Input from "./Input";
 import { text } from "@storybook/addon-knobs";
+import { GiMassDriver } from "react-icons/gi";
 
 export default {
   title: "Design/Atoms/Input",
   component: Input,
 };
 
-const Template = (args) => <Input {...args} value={text("Text", "DUPSKO")} />;
+const Template = (args) => <Input {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = { width: "500px" };
+
+export const Icon = Template.bind({});
+Icon.args = {
+  icon: GiMassDriver,
+};
